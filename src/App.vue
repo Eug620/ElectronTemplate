6f88c12897100ec2718dd34a631fc732b11db270
yeyuhang <yeyuhang@appicplay.com>
2021-06-04 18:29:40 +0800

    Init Project

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<!--
+ * @Author       : Eug
+ * @Date         : 2021-06-04 18:11:15
+ * @LastEditTime : 2021-06-04 18:25:28
+ * @LastEditors  : Eug
+ * @Descripttion : Descripttion
+ * @FilePath     : /vue3-electron/src/App.vue
+-->
+<script>
+import { computed, ref } from 'vue'
+export default {
+  name: 'App',
+  setup() {
+    const HelloWrold = ref('Hello World!')
+    const useCompHW = computed(() => HelloWrold.value)
+    return {
+      HelloWrold,
+      useCompHW
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container mt-2">
+    {{ useCompHW }}
+  </div>
+</template>
